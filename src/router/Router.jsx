32e4e430b0c389a9f7ede4dad165520fe611d7b2
ShafiurMiju <srmmiju@gmail.com>
2025-02-05 17:54:43 +0600

    feat: update routing configuration to use rewrites for improved handling of paths

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,38 +1,44 @@
-import { createBrowserRouter } from "react-router-dom";
-import ProtectedRoute from "../auth/ProtectedRoute";
-import AuthRedirect from "../auth/AuthRedirect";
-import LoginForm from "../pages/LoginForm";
-import LoginGHL from "../pages/LoginGHL";
-import OwnerPortfolioView from "../pages/OwnerPortfolioView";
-import PropertySearch from "../pages/PropertySearch";
-import DealCheck from "../pages/DealCheck";
-import NotFound from "../pages/NotFound";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <AuthRedirect element={<LoginForm />} />,
-  },
-  {
-    path: "/ghl",
-    element: <AuthRedirect element={<LoginGHL />} />,
-  },
-  {
-    path: "/property-search",
-    element: <ProtectedRoute element={<PropertySearch />} />,
-  },
-  {
-    path: "/property",
-    element: <ProtectedRoute element={<OwnerPortfolioView />} />,
-  },
-  {
-    path: "/dealCheck",
-    element: <ProtectedRoute element={<DealCheck />} />,
-  },
-  {
-    path: "/*",
-    element: <ProtectedRoute element={<NotFound />} />,
-  },
-]);
-
-export default router;
+{
+  "rewrites": [
+    {
+      "source": "/ghl",
+      "destination": "/index.html"
+    },
+    {
+      "source": "/ghl/:path*",
+      "destination": "/index.html"
+    },
+    {
+      "source": "/property-search",
+      "destination": "/index.html"
+    },
+    {
+      "source": "/property-search/:path*",
+      "destination": "/index.html"
+    },
+    {
+      "source": "/property",
+      "destination": "/index.html"
+    },
+    {
+      "source": "/property/:path*",
+      "destination": "/index.html"
+    },
+    {
+      "source": "/dealCheck",
+      "destination": "/index.html"
+    },
+    {
+      "source": "/dealCheck/:path*",
+      "destination": "/index.html"
+    },
+    {
+      "source": "/",
+      "destination": "/index.html"
+    },
+    {
+      "source": "/:path*",
+      "destination": "/index.html"
+    }
+  ]
+}
